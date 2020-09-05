eee9402086d60e7083c84c654f1b2a78dd9bb8f4
Moein <mohammadmoein.moradi@gmail.com>
2020-09-05 21:55:16 +0430

    Updated words database and sending fetch request for word pronunciation

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const exhbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server is running at port ${port}`));
+
+// database
+const Datastore = require('nedb');
+const usersDatabase = new Datastore({
+  filename: path.join(__dirname, 'database', 'users-database.db'),
+  autoload: true,
+});
+const wordsDatabase = new Datastore({
+  filename: path.join(__dirname, 'database', 'words-database.db'),
+  autoload: true,
+});
+console.log('Databasae loaded');
+
+module.exports = { usersDatabase, wordsDatabase };
+
+// temp user
+app.use(tempUser);
+function tempUser(req, res, next) {
+  req.user = 1;
+  next();
+}
 
 // html form middleware
 app.use(express.urlencoded({ extended: false }));
--- routes/game.js
+++ routes/game.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
+require('dotenv').config();
 
-router.get('/', (req, res) => {
-  res.render('game');
+const { usersDatabase, wordsDatabase } = require('../app.js');
+
+router.get('/', async (req, res) => {
+  try {
+    const word = 'producer';
+    const apiURL = `https://api.wordnik.com/v4/word.json/${word}/audio?useCanonical=false&limit=1&api_key=${process.env.APP_KEY}`;
+    const apiRes = await fetch(apiURL);
+    const data = await apiRes.json();
+    const audioURL = data[0].fileUrl;
+
+    console.log(audioURL);
+
+    res.render('game');
+  } catch (error) {
+    throw error;
+  }
 });
 
 router.post('/', (req, res) => {
