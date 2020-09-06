0089fec3022d07b6de518902442349b686fc1bfa
Moein <mohammadmoein.moradi@gmail.com>
2020-09-06 13:50:38 +0430

    Added logic of the game to get streak and lose if the dictarion was wrong

--- routes/game.js
+++ routes/game.js
@@ -1,28 +1,49 @@
+let userLevel = 1; // initial user streak
+let currentWord;
+
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 require('dotenv').config();
 
 const { usersDatabase, wordsDatabase } = require('../app.js');
+// wordsDatabase.findOne({ wordTag: userLevel.toString() }, (err, docs) =>
+//   console.log(docs)
+// );
 
 router.get('/', async (req, res) => {
-  try {
-    // fetch request for word pronunciation
-    const word = 'apple';
-    const apiURL = `https://api.wordnik.com/v4/word.json/${word}/audio?useCanonical=false&limit=1&api_key=${process.env.APP_KEY}`;
-    const apiRes = await fetch(apiURL);
-    const data = await apiRes.json();
-    const audioURL = data[0].fileUrl;
+  wordsDatabase.findOne({ wordTag: userLevel.toString() }, async (err, doc) => {
+    if (err) console.log(err);
+    const word = (currentWord = doc.word);
+    try {
+      const apiURL = `https://api.wordnik.com/v4/word.json/${word}/audio?useCanonical=false&limit=1&api_key=${process.env.APP_KEY}`;
+      const apiRes = await fetch(apiURL);
+      const data = await apiRes.json();
 
-    res.render('game', { jsFile: 'game.js', audioSrc: audioURL });
-  } catch (error) {
-    throw error;
-  }
+      const audioURL = data[0].fileUrl;
+
+      res.render('game', {
+        jsFile: 'game.js',
+        audioSrc: audioURL,
+        currentLevel: userLevel,
+      });
+    } catch (error) {
+      console.log('Error on catch phrase get /game route');
+      console.log(error);
+      res.redirect('/');
+    }
+  });
 });
 
 router.post('/', (req, res) => {
   console.log(req.body.word);
+  if (req.body.word == currentWord) userLevel++;
+  else restartUserLevel();
   res.redirect('/game');
 });
+
+function restartUserLevel() {
+  userLevel = 1;
+}
 
 module.exports = router;
