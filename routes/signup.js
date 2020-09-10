594445f87fb78fd876c8fb17dbfbee4fe0e54e55
Moein <mohammadmoein.moradi@gmail.com>
2020-09-10 12:17:21 +0430

    won't allow signing-up with existing username

--- routes/signup.js
+++ routes/signup.js
@@ -30,34 +30,56 @@ router.post('/', isNotAuthenticated, (req, res) => {
         res.redirect('signup');
       }
 
-      if (userFoundWithEmail != null) {
-        // there's a user with same email
-        renderObj.signupErrors.push('This email already exists!');
-        renderObj.email = null;
-        renderObj.username = req.body.username;
-        renderObj.isUserLogged = req.isAuthenticated();
-        renderObj.username = req.isAuthenticated() ? req.user.username : null;
-      }
-      if (renderObj.signupErrors.length != 0) {
-        res.render('signup', renderObj);
-      } else {
-        // inserting new user to database
-        const { email, username, password } = req.body;
-        // initialize new user
-        const user = {
-          email,
-          username,
-          password: bcrypt.hashSync(password, 10),
-          userLevel: 1,
-        };
-        usersDatabase.insert(user);
+      // check if the input username is already exists
+      usersDatabase.findOne(
+        { username: req.body.username },
+        (err, userFoundwithUsername) => {
+          if (err) {
+            console.log('Error in signup post request');
+            console.log(err);
+            res.redirect('signup');
+          }
 
-        req.flash('appMsg', 'You may now sign-in with your account');
-        res.render('signin', {
-          isUserLogged: req.isAuthenticated(),
-          username: req.isAuthenticated() ? req.user.username : null,
-        });
-      }
+          if (userFoundWithEmail != null) {
+            // there's an user with same email
+            renderObj.signupErrors.push('This email already exists!');
+            renderObj.email = null;
+            renderObj.username = req.body.username;
+            renderObj.isUserLogged = req.isAuthenticated();
+            renderObj.username = req.isAuthenticated()
+              ? req.user.username
+              : null;
+          }
+
+          if (userFoundwithUsername != null) {
+            // ther's an user with same username
+            renderObj.signupErrors.push('This username already exists!');
+            renderObj.email = userFoundWithEmail ? null : req.body.email;
+            renderObj.username = null;
+          }
+
+          if (renderObj.signupErrors.length != 0) {
+            res.render('signup', renderObj);
+          } else {
+            // inserting new user to database
+            const { email, username, password } = req.body;
+            // initialize new user
+            const user = {
+              email,
+              username,
+              password: bcrypt.hashSync(password, 10),
+              userLevel: 1,
+            };
+            usersDatabase.insert(user);
+
+            req.flash('appMsg', 'You may now sign-in with your account');
+            res.render('signin', {
+              isUserLogged: req.isAuthenticated(),
+              username: req.isAuthenticated() ? req.user.username : null,
+            });
+          }
+        }
+      );
     }
   );
 });
